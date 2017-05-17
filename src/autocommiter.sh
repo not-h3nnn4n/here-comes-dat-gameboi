2fa4ad4aba048d081d1e21295f2621f751c1dfca
not_h3nnn4n <not.h3nnn4n@gmail.com>
2017-05-17 11:10:46 -0300

    "Autocommit at 17_05_11_10_46"

--- src/autocommiter.sh
+++ src/autocommiter.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+TIMER=2
 
+while true
+do
+	sleep $TIMER
+	now=$(date "+%d_%m_%H_%M_%S")
+	msg='"Autocommit at '$now\"
+	echo $msg
+	git add --all
+	git commit -m "$msg"
+	git push
+done
