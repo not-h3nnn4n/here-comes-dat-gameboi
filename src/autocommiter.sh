a994c6020f507d8dc91043342b34daa03e137af7
not_h3nnn4n <not.h3nnn4n@gmail.com>
2017-05-17 11:13:26 -0300

    "Autocommit at 17_05_11_13_26"

--- src/autocommiter.sh
+++ src/autocommiter.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-TIMER=2
+TIMER=600 # 30 minutes
 
 while true
 do
-	sleep $TIMER
 	now=$(date "+%d_%m_%H_%M_%S")
 	msg='"Autocommit at '$now\"
 	echo $msg
 	git add --all
 	git commit -m "$msg"
 	git push
+	sleep $TIMER
 done
